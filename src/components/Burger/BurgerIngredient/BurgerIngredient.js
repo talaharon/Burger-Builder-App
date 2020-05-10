aa22ad8f022ce968543296d01e112c02770f1fc9
Tal Aharon <taharon@google.com>
2020-05-10 15:57:26 +0300

    Outputting Burger Ingredients Dynamically

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -28,6 +28,7 @@ class BurgerIngredient extends Component{
                 break;
             case ('bacon'):
                 ingredient = <div className = {classes.Bacon}></div>;
+                break;
             default:
                 ingredient = null;
         }
