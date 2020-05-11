1c3939dd7ca28daaaaecbbe3168b27d7d8850ea3
Tal Aharon <taharon@google.com>
2020-05-11 14:57:18 +0300

    More function added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+
+const INGREDIENT_PRICES = {
+        salad: 0.5,
+        cheese: 0.4,
+        meat: 1.3,
+        bacon: 0.7
+}
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -10,13 +17,30 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0,
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = (type) => {
+        const newIngredients = {...this.state.ingredients};
+        newIngredients[type] = newIngredients[type]+1;
+        const totalNewPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({
+            ingredients: newIngredients,
+            totalPrice: totalNewPrice
+        });
+    };
+
+    removeIngredientHandler = (type) => {
+
+    };
+
     render() {
         return (
             <Aux>
                 <Burger ingredients = {this.state.ingredients}></Burger>
-                <BuildControls></BuildControls>
+                <BuildControls 
+                ingredientAdded = {this.addIngredientHandler}/>
             </Aux>
         );
     }
